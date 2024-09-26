b77e56312de7f095d630dcaabf27d38b429bc17e
Javier Alfaro <ojalfarob@gmail.com>
2024-09-26 09:13:12 -0600

    add db conection with postgresql

--- inventario/settings.py
+++ inventario/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'inventarios'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'inventario.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Producto',
+        'USER': 'postgres',
+        'PASSWORD':'12345',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
